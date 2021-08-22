e3b201dca518a9002f868469c5a454426e60231b
Paul McDoyen <mrpoloh@yahoo.co.uk>
2021-08-22 11:05:36 +0300

    Convert js to ts

---
+++ src/App.ts
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { createElement } from "react";
+
+function App() {
+  return createElement(
+    "div",
+    { className: "App" },
+    createElement(
+      "header",
+      { className: "App-header" },
+      createElement("img", { className: "App-logo", src: logo }),
+      createElement(
+        "p",
+        {},
+        "Edit ",
+        createElement("code", {}, "src/App.js"),
+        " and save to reload."
+      ),
+      createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      )
+    )
+  );
+}
+
+export default App;
