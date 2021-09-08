c73811667365fc7d22f10138f70b04c734d3ead2
Paul McDoyen <mrpoloh@yahoo.co.uk>
2021-09-08 15:44:36 +0300

    Fetch location coordinates

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { createElement } from "react";
-import LocationIcon from '@material-ui/icons/LocationOn';
+import { createElement, useEffect, useState } from "react";
+import LocationIcon from "@material-ui/icons/LocationOn";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    if (navigator) {
+      navigator.geolocation.watchPosition((position) => {
+        setCurrentLocation(position.coords);
+        console.log(position.coords);
+      });
+    } else {
+      console.log("location access denied");
+    }
+  }, [currentLocation]);
+
   return createElement(
     "div",
     { className: "App" },
@@ -12,7 +29,12 @@ function App() {
       "header",
       { className: "App-header" },
       createElement("p", {}, "Weather app"),
-      createElement(LocationIcon),
+      createElement(LocationIcon, {}),
+      createElement(
+        Typography,
+        {},
+        `${currentLocation.longitude} ${currentLocation.latitude}`
+      ),
       createElement("img", { className: "App-logo", src: logo }),
       createElement(
         "p",
