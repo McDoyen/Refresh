fdfae85b3acb50ca067ba18c01fe6adf9c9426d5
Paul McDoyen <mrpoloh@yahoo.co.uk>
2021-09-12 19:55:30 +0300

    Setup leaflet maps

--- src/App.ts
+++ src/App.ts
@@ -1,53 +1,120 @@
 import { createElement, useEffect, useState } from "react";
+
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import LocationIcon from "@material-ui/icons/MyLocation";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import axios from "axios";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "./App.css";
+import 'leaflet/dist/leaflet.css';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    currentGrid: { paddingLeft: "25%" },
+    grid: {
+      textAlign: "left",
+      width: "50%",
+    },
+    root: {
+      margin: "60px 60px 60px 60px",
+    },
+    tempGrid: {
+      display: "contents",
+    },
+  })
+);
 
 function App() {
   const [{ city, country }, setState] = useState({ city: "", country: "" });
-  const [{ temperature, weatherDiscription, weatherIcon }, setData] = useState({
-    temperature: 0,
-    weatherDiscription: "",
-    weatherIcon: "",
-  });
+  const [{ feelsLike, temperature, weatherDiscription, weatherIcon }, setData] =
+    useState({
+      feelsLike: 0,
+      temperature: 0,
+      weatherDiscription: "",
+      weatherIcon: "",
+    });
 
+  const classes = useStyles();
   const apiKey = process.env.REACT_APP_API_KEY;
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+
+  const openWeatherApiURL = axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
+  );
+  const openWeatherMapApiURL = axios.get(
+    `https://tile.openweathermap.org/map/temp_new/5/5/5.png?appid=${apiKey}`
+  );
   const getLocation = "http://ip-api.com/json/";
+
+  let time = new Date();
 
   useEffect(() => {
     axios.get(getLocation).then(({ data }) => {
       setState({ city: data.city, country: data.country });
     });
-    axios.get(apiURL).then(({ data }) => {
-      setData({
-        temperature: data.main.temp,
-        weatherDiscription: data.weather[0].description,
-        weatherIcon: data.weather[0].icon,
-      });
-    });
+    axios.all([openWeatherMapApiURL, openWeatherApiURL]).then(
+      axios.spread((...responses) => {
+        setData({
+          feelsLike: responses[1].data.main.feels_like,
+          temperature: responses[1].data.main.temp,
+          weatherDiscription: responses[1].data.weather[0].description,
+          weatherIcon: responses[1].data.weather[0].icon,
+        });
+      })
+    );
   }, [city, temperature]);
 
   return createElement(
     "div",
-    { className: "App" },
+    { className: classes.root },
     createElement(
-      "header",
-      { className: "App-header" },
-      createElement("p", {}, "Weather app"),
+      Grid,
+      { container: true },
       createElement(
-        "div",
-        { className: "Location" },
+        Grid,
+        { item: true, xs: 12 },
         createElement(LocationIcon, {}),
-        createElement(Typography, {}, `${city}, ${country}`),
-        createElement(Typography, {}, temperature),
-        createElement(Typography, {}, weatherDiscription)
+        createElement(Typography, {}, `${city}, ${country}`)
       ),
-      createElement("img", {
-        src: `http://openweathermap.org/img/w/${weatherIcon}.png`,
-      })
+      createElement(
+        Grid,
+        { className: classes.tempGrid, item: true, xs: 12, sm: 6 },
+        createElement(
+          Grid,
+          { item: true, xs: 6, sm: 3 },
+          createElement(Typography, {}, "CURRENT WEATHER"),
+          createElement(Typography, {}, time.toLocaleTimeString()),
+          createElement("img", {
+            src: `http://openweathermap.org/img/w/${weatherIcon}.png`,
+          }),
+          createElement(Typography, {}, `${temperature}°`)
+        ),
+        createElement(
+          Grid,
+          { item: true, xs: 6, sm: 3 },
+          createElement(Typography, {}, weatherDiscription),
+          createElement(Typography, {}, `Feels like ${feelsLike}°`)
+        )
+      ),
+      createElement(
+        Grid,
+        {item: true, xs: 12, sm: 6 },
+        createElement(
+          MapContainer,
+          {
+            center: [51.505, -0.09],
+            zoom: 13,
+            scrollWheelZoom: false,
+          },
+          createElement(TileLayer, {
+            attribution:
+              '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          }),
+          createElement(Marker, { position: [51.505, -0.09] })
+        )
+      )
     )
   );
 }
